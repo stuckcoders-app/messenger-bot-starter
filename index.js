bb1e2db1e50492cfbee1a68311a1801a1628fab2
Oshomo Oforomeh <oshomo.oforomeh@konga.com>
2016-04-13 16:37:18 +0100

    Added method to get callbacks from users

--- index.js
+++ index.js
@@ -30,6 +30,25 @@ app.get('/webhook', function (req, res) {
   }
 });
 
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+	
+	if (event.postback) {
+		text = JSON.stringify(event.postback);
+		sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
+		continue;
+	}
+  }
+  res.sendStatus(200);
+});
+
 app.listen(port, function() {
     console.log('Our app is running on port ' + port);
 });
