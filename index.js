b4adaa5f16bdbee92d798bbdc06a6be2c7ee60ad
Oshomo Oforomeh <oshomo.oforomeh@konga.com>
2016-04-13 15:37:23 +0100

    Added body-parser for get body

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 var cons = require("consolidate");
+var bodyParser = require("consolidate");
 
 var port = process.env.PORT || 3000;
 var access_token = process.env.ACCESS_TOKEN;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
