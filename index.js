bba0f61e41c3e2cb339e78251830fbb204f1496e
Oshomo Oforomeh <oshomo.oforomeh@konga.com>
2016-04-14 09:55:43 +0100

    Resolved conflict

--- index.js
+++ index.js
@@ -1,99 +1,46 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-
+var con = require('consolidate');
 var app = express();
-
+var page_token = process.env.PAGE_TOKEN;
 
 
 // instruct the app to use the `bodyParser()` middleware for all routes
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-
 app.use(bodyParser.json());
-
-var token = process.env.PAGE_TOKEN;
-function sendGenericMessage(sender) {
-	messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com/",
-						"title": "Web url"
-					}, {
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				},{
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
-			}
-		}
-	};
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending message: ', error);
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error);
-		}
-	});
-}
-
-function sendTextMessage(sender, text) {
-	messageData = {
-		text:text
-	}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending message: ', error);
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error);
-		}
-	});
-}
-
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-	response.render('pages/index');
+function sendMessage(sender, messageData) {
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: page_token
+		},
+		method: 'POST',
+		json: {
+			recipient: {
+				id: sender
+			},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending message: ', error);
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+		}
+	});
+}
+
+app.get('/privacy-policy', function(request, response) {
+	response.render('privacy');
 });
 
 app.get('/webhook/', function (req, res) {
@@ -102,25 +49,33 @@ app.get('/webhook/', function (req, res) {
 	}
 	res.send('Error, wrong validation token');
 });
+
 app.post('/webhook/', function (req, res) {
-
-	messaging_events = req.body.entry[0].messaging;
-	for (i = 0; i < messaging_events.length; i++) {
-		event = req.body.entry[0].messaging[i];
-		sender = event.sender.id;
-
-		if (event.message && event.message.text) {
-			text = event.message.text;
-
-			if (text === 'Generic') {
-				sendGenericMessage(sender);
-				continue;
-			}
-
-			sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+  messaging_events = req.body.entry[0].messaging;
+  console.log(req.body.entry);
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      var text = event.message.text;
+	  var response = {
+				text: "This you say" + text
+			};
+      // Handle a text message from this sender
+	  sendMessage(sender, response);
+	  
+    }else if (event.postback) {
+		var postback_text = event.postback.payload;
+		if (postback_text == "USER_REQUEST_SHIPPING_PRICE") {
+			var message = {
+				text: "Am on it...What state are you shipping from?"
+			};
+			sendMessage(sender, message);
+			continue;
 		}
 	}
 	res.sendStatus(200);
+  }
 });
 
 app.listen(app.get('port'), function() {
